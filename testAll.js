8a475dbcdeb355461615e990a692fea7c626d7c2
vigubik <vigubik@gmail.com>
2016-08-25 10:12:40 +0300

    add tests

---
+++ testAll.js
@@ -0,0 +1,9 @@
+﻿var expect = require("chai").expect;
+var all = require('./all.js');
+
+describe('combineExecutionArgs', function () {
+   it('combines', function() {
+      var combined = all.combineExecutionArgs(['initial'], {key: function(arg) { return arg; } }, 'key', ['arg']);
+      expect(combined).to.deep.equal(['initial','arg']);
+   });
+})
